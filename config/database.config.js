17a3b1b38b2e40c3d24bfba93720ef895d40b32e
Mehdi <Hussain>
2020-05-26 20:47:43 -0400

    Initial commit

---
+++ config/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://admin-getdone:S3rVer4pAss@truemongo-shard-00-00-dyxhw.mongodb.net:27017,truemongo-shard-00-01-dyxhw.mongodb.net:27017,truemongo-shard-00-02-dyxhw.mongodb.net:27017/test?ssl=true&replicaSet=TrueMongo-shard-0&authSource=admin&retryWrites=true&w=majority"
+  };
